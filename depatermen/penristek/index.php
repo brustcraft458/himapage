7ca8d3c8b4fd95bfc773d2c40d0582747dfa512b
brustcraft458 <brustcraft458@gmail.com>
2024-02-25 23:47:38 +0700

    Update Navbar

--- depatermen/penristek/index.php
+++ depatermen/penristek/index.php
@@ -25,22 +25,7 @@ $rootdir = "../..";
 </head>
 
 <body>
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
-    <div class="container d-flex align-items-center">
-      <h1 class="logo me-auto"><a href="index.html">Himatif</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="<?= $rootdir ?>/assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav id="navbar" class="navbar">
-        <ul>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav>
-      <!-- .navbar -->
-    </div>
-  </header>
-  <!-- End Header -->
+  <?php require_once("$rootdir/assets/component/header.php"); ?>
 
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
